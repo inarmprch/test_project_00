0199eb792726afa186c0d65195d8662053d0dfdc
inarmprch <phuricha.rom@gmail.com>
2025-01-26 08:10:45 +0000

    New Column

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ st.set_page_config(layout="wide")
 st.title('Test Streamlit')
 
 col1_1, col2_2 = st.columns(2)
-with col1_1;
+with col1_1:
     st.header("AAAAAA")
-with col1_2;
+with col1_2:
     st.header("BBBBBB")
 
 col2_1, col2_2 = st.colimns([3,1], border=True)
